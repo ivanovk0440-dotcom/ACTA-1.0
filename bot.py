85b0b6ae4fef7465bbc584a0379e13035cc67f54
ivanovk0440-dotcom <ivanovk0440@gmail.com>
2026-06-25 02:25:44 +0300

    Update ci

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Mess
 # КОНФИГУРАЦИЯ — ЗАМЕНИТЕ НА СВОИ ЗНАЧЕНИЯ
 # ═══════════════════════════════════════════════════════════
 BOT_TOKEN = "8840512956:AAEkwdKYMSl56kryZ-JgPXYWrBjvJ5EPoAU"
-WEBAPP_URL = "acta101-c0dsno1ab-ivanovk0440-1326s-projects.vercel.app"  # например: https://your-app.vercel.app
+WEBAPP_URL = "https://acta11d-dpq1hcvkd0x9.edgeone.dev"  # например: https://your-app.vercel.app
 
 # Логирование
 logging.basicConfig(
